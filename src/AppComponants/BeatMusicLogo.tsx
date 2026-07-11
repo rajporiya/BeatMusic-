590f5b39077409ea85a01b37e8206fef2ddc1fb4
Rajporiya <mrraj4129@gmail.com>
2026-07-11 20:14:31 +0530

    Beat Music init

---
+++ src/AppComponants/BeatMusicLogo.tsx
@@ -0,0 +1,9 @@
+const BeatMusicLogo = ({className= ""}) => {
+  return (
+    <div className={className}>
+      <img src="./Animation/title.svg" alt=""  className=''/>
+    </div>
+  )
+}
+
+export default BeatMusicLogo
---
+++ src/AppComponants/Navbar.tsx
@@ -0,0 +1,62 @@
+import BeatMusicLogo from "./BeatMusicLogo"
+import { Button } from "@/components/ui/button"
+import { Bell, House, Search, Users } from "lucide-react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group"
+import { Link } from "react-router-dom"
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+const Navbar = () => {
+  return (
+  <div className="flex items-center   ">
+
+    {/* Left */}
+    <div className="flex-1 flex justify-start">
+      <BeatMusicLogo className="w-8" />
+    </div>
+
+    {/* Center */}
+    <div className="flex-1 flex justify-center items-center gap-2">
+      <Button className="h-9 w-9 rounded-full">
+        <House className="h-5 w-5" />
+      </Button>
+
+      <InputGroup className="max-w-[450px]">
+        <InputGroupInput placeholder="Search..." />
+        <InputGroupAddon>
+          <Search />
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+
+    {/* Right */}
+    <div className="flex-1 flex justify-end items-center gap-6">
+      <Link to="/">Explore Premium</Link>
+
+      <Link to="/">Install App</Link>
+
+      <Bell className="cursor-pointer" />
+
+      <Users className="cursor-pointer" />
+
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+          alt="@shadcn"
+        />
+      </Avatar>
+    </div>
+
+  </div>
+);
+}
+
+export default Navbar
